0b525e7d00cfe2332d605f493d4cbb2f7d817260
pnbakken <74681160+pnbakken@users.noreply.github.com>
2025-05-21 13:19:13 +0200

    Add files via upload

--- salon.sh
+++ salon.sh
@@ -1,7 +1,50 @@
 #!/bin/bash
 # Salon appointment booker
 
-PSQL="psql --username=freecodecamp --dbname=salon -t --no-align -c "
+PSQL="psql --username=freecodecamp --dbname=salon -t -c "
 
 TEST_RESULT=$($PSQL "SELECT * FROM SERVICES")
 echo $TEST_RESULT
+
+echo -e "\n~~~~~ MY SALON ~~~~~\n"
+
+MAIN_MENU() {
+
+  if [[ $1 ]]
+  then 
+    echo "$1"
+  fi
+
+  echo -e "\nWelcome to My Salon, how can I help you?\n"
+
+  GET_SERVICES=$($PSQL "SELECT * FROM services")
+  echo "$GET_SERVICES" | while read ID BAR SERVICE
+  do
+    echo "$ID) $SERVICE" 
+  done
+
+  read SERVICE_CHOICE
+  
+  BOOK_APPOINTMENT $SERVICE_CHOICE
+
+
+}
+
+BOOK_APPOINTMENT() {
+
+  echo -e "Booking appointment"
+  echo -e "Chosen service: $1"
+
+  CHECK_SERVICE=$($PSQL "SELECT name FROM services WHERE service_id = $1")
+
+  if [[ -z $CHECK_SERVICE ]]
+  then 
+    MAIN_MENU "That service does not exist"
+  else
+    # Do booking
+    echo -e "\n"
+  fi
+}
+
+
+MAIN_MENU
